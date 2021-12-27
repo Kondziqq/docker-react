0a1c11cbf5203e25b71bc849ddf6ae6775f59c6f
Konrad Jarmuszkiewicz <kjarmuszkiewicz@sii.pl>
2021-12-27 12:32:27 +0100

    Test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Myszom Ty pierdzielcu!
+          Hello World!
         </p>
       </header>
     </div>
